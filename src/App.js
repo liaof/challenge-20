ba12a7160b804e3b70a3b82f8946f0f0b02995b8
liaof <f.liao163@gmail.com>
2021-08-08 11:39:10 -0400

    react-pdf

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Contact from './components/Contact';
 import Header from './components/Header';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
-import { capitalizeFirstLetter } from './utils/helpers';
-
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function App() {
 
@@ -36,7 +36,12 @@ function App() {
         ) : currentCategory==='resume'? (
           <section className="my-5 content-wrap">
             <h2 className='py-1'>Resume</h2>
-            <a href = 'https://www.google.com/search?q=resume&sxsrf=ALeKk03XFcsAaGbiQzlOhJVXibU-f1hjFw%3A1626057954330&source=hp&ei=4qzrYN3bEZS4tQb82pDgBA&iflsig=AINFCbYAAAAAYOu68tloyreR2Q5_xh8c42FMhq0g1l3U&oq=resume&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEEMyBwgAELEDEEMyBAgAEEMyBwgAELEDEEMyBAgAEEMyBwgAELEDEEMyBwgAELEDEEMyBwgAELEDEEMyBAgAEEMyBQgAELEDOgcIIxDqAhAnOgQIIxAnOgYIABAKEEM6BwguELEDEEM6AggAUN4MWPERYMsSaAFwAHgAgAFyiAHCBJIBAzMuM5gBAKABAaoBB2d3cy13aXqwAQo&sclient=gws-wiz&ved=0ahUKEwjd1P_1wdzxAhUUXM0KHXwtBEwQ4dUDCAk&uact=5'>And here I would insert the link to my resume, If I had a proper one at this current time</a>
+            <Document
+              file='/Resume.pdf'
+            >
+              <Page></Page>
+            </Document>
+
           </section>
         ) : (
           <Contact></Contact>
